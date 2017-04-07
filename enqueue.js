b80d5f7dc2c7d81710f4c46bb15ae9523cff445d
slashhuang <627284482@qq.com>
2017-04-07 11:36:29 +0800

    add optimized 100 uid length restriction

--- enqueue.js
+++ enqueue.js
@@ -3,7 +3,7 @@
 
 // uidList 是一个数组，最大接受 100 个 uid
 // 这个方法的实现不能修改
-var requestUserProfile = function(uidList){  
+var requestUserProfile = function(uidList){
   /** 先去重 */
   var uidList = uidList || [];
   var _tmp = {};
@@ -40,12 +40,12 @@ var requestUserProfile = function(uidList){
   });
 }
 
-// 现在我们有很多业务都需要根据 uid 获取 userProfile , 
+// 现在我们有很多业务都需要根据 uid 获取 userProfile ,
 // 大多数业务的需求都是给一个 uid，获取 profile 。
 // 为了性能，我们需要把这个单个的请求合并成批量请求。
 
 // 例如，现在页面上 A 模块需要获取 uid 为 1 的 profile，
-// B 模块需要 uid 为 2 的 profile， 
+// B 模块需要 uid 为 2 的 profile，
 // C 模块需要获取 uid 为 1 的profile
 // 这三个模块会单独调用下面这个方法获取 profile，
 // 假设这三次调用的时间非常接近(100ms 以内)，最终要求只发送一个 ajax 请求（
@@ -54,8 +54,11 @@ var requestUserProfile = function(uidList){
 // 完成以下方法，接收一个参数 uid，返回一个 Promise，
 // 当成功请求到 profile 的时候， resolve 对应的profile , 请求失败 reject
 // 例如  getUserProfile(1).then(function(profile){ console.log(profile.uid === 1) // true });  // 假设请求成功了。
-var getUserProfile = function(uid){
-  /* 
+var getUserProfile = function(TaskInterval){
+  if(!TaskInterval){
+    TaskInterval=100
+  }
+  /*
    * @Author slashhuang
    * 17/4/6
    */
@@ -63,66 +66,54 @@ var getUserProfile = function(uid){
    let enqueueUid = [];
    let enqueueTask = [];
    //任务等待区域
-   let waitingTask = [];
-   
    let timer = null;
-   return (uid)=>{
-      //推送进队列
-      enqueueUid.push(uid);
-      //任务时间管理
-      if(!timer){
-        timer = setTimeout(()=>{
-            clearTimeout(timer);
-            timer = null;
-            //进入任务等待区
-            let dispatchUid  = enqueueUid;
-            let dispatchTask = enqueueTask;
-            //生成一组任务
-            let currentTask = {
-              'dispatchTask':dispatchTask,
-              'dispatchUid':dispatchUid
-            };
-            waitingTask.push(currentTask);
-            //清空缓存区
-            enqueueTask =[];
-            enqueueUid = [];
-            dispatchUidTask(currentTask);
-          },100)
-      }else{
-        console.log(`task pushed to queue ,stay in pending state`)
-      }
-      //删除执行完的队列
-      let deleteTask=(taskArr,task)=>{
-          let index = taskArr.indexOf(task);
-          taskArr.splice(index,1);
-      }
-      //通知任务队列执行
-      let notify = currentTask=>list=>{
-          // task要和uid对应起来
-          currentTask.dispatchTask.forEach(task=>task(list));
-          deleteTask(waitingTask,currentTask);
-      }; 
-      //合并uid请求
-      let dispatchUidTask=(currentTask)=>{
-          let dispatchUid = currentTask['dispatchUid'];
-          let dispatchTask = currentTask['dispatchTask'];
-          Promise.resolve({then:(resolve,reject)=>{
-                    resolve(dispatchUid);
-                }})
-                .then(requestUserProfile)
-                .then(notify(currentTask))
+   let runTask = ()=>{
+        //清空定时器
+        timer && clearTimeout(timer);
+        timer = null;
+        //执行当前任务
+        dispatchUidTask({
+              'TaskArr':[].slice.call(enqueueTask),
+              'dispatchUid':[].slice.call(enqueueUid)
+        });
+        //清空缓存区
+        enqueueTask =[];
+        enqueueUid = [];
+   };
+    //合并uid请求
+    let dispatchUidTask=(currentTask)=>{
+        let dispatchUid = currentTask['dispatchUid'];
+        let TaskArr = currentTask['TaskArr'];
+        Promise.resolve(dispatchUid)
+               .then(requestUserProfile)
+               //通知任务队列执行
+               .then(list=>TaskArr.forEach(task=>task(list)))
         }
-      //过滤返回结果
-      let filterResult = uid=>list=>{
-            return list.filter(profile=>profile.uid == uid)[0];
-        }  
-      //组装Promise逻辑
+    let loadTask = (uid)=>{
+        //满100就执行任务
+        if(enqueueUid.length>99){
+              console.log('------- length > 100')
+              runTask()
+        }else if(!timer){
+            timer = setTimeout(()=>{
+                console.log('------- starting setTimeout task')
+                runTask()
+            },TaskInterval)
+        }
+    }
+   return (uid)=>{
+      //返回Promise任务，将resolve推送进enqueueTask队列
       return new Promise((resolve,reject)=>{
-            //夺取Promise控制权
+            //推送任务列表和uid列表
             enqueueTask.push(resolve);
-        }).then(filterResult(uid))
-          .catch(error=>{ //只处理真实的错误
-              if(error.name){
+            enqueueUid.push(uid);
+            //加载任务
+            loadTask(uid);
+        }).then((list)=>{
+            return list.filter(profile=>profile.uid == uid)[0];
+          })
+          .catch(error=>{
+              if(error && error.stack){
                   return Promise.reject(`${error.name}--${error.stack} `)
               }else{
                   return Promise.reject(error)
@@ -131,51 +122,24 @@ var getUserProfile = function(uid){
     }
 };
 //-------------------------- 测试区 ------------------------------------
-let userProfilePayLoad = getUserProfile();
+//110毫秒的任务间隔
+let TaskNum = 105;
+let TaskInterval = 110;
+let userProfilePayLoad = getUserProfile(TaskInterval);
 //模拟异步请求
-let async = (fn,gap)=>{
+let _async = (fn,gap)=>{
   setTimeout(fn,gap)
+}
+while(TaskNum>0){
+  (taskNum=> _async(()=>{
+    /*
+     * 打印1到105
+     * 任务执行次数2次
+     */
+    userProfilePayLoad(taskNum).then(profile=>console.log(profile.uid)).catch(console.log)
+  },TaskNum))(TaskNum)
+  TaskNum--
 };
-async(()=>{
-  //打印10
-  userProfilePayLoad(10).then(profile=>console.log(profile.uid)).catch(console.log)
-},10)
-async(()=>{
-   //打印20
-  userProfilePayLoad(20).then(profile=>console.log(profile.uid)).catch(console.log);
-},20)
-async(()=>{
-   //打印30
-  userProfilePayLoad(30).then(profile=>console.log(profile.uid)).catch(console.log);
-},30)
-async(()=>{
-   //打印100
-  userProfilePayLoad(100).then(profile=>console.log(profile.uid)).catch(console.log);
-},100)
-async(()=>{
-   //打印150
-  userProfilePayLoad(150).then(profile=>console.log(profile.uid)).catch(console.log);
-},150)
-async(()=>{
-   //打印200
-  userProfilePayLoad(200).then(profile=>console.log(profile.uid)).catch(console.log);
-},200)
-async(()=>{
-   //打印400
-  userProfilePayLoad(400).then(profile=>console.log(profile.uid)).catch(console.log);
-},400)
-async(()=>{
-   //打印1000
-  userProfilePayLoad(1000).then(profile=>console.log(profile.uid)).catch(console.log);
-},1000)
-
-
-
-
-
-
-
-
 
 
 
